e3f31a17eb539bfbb7391000b70bc4d65a1c9023
Alexandre Wotawa <awotawa@gmail.com>
2021-02-17 12:52:59 +0100

    Add backspace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
+
   # Store uploaded files on Amazon AWS
   config.active_storage.service = :amazon
 
